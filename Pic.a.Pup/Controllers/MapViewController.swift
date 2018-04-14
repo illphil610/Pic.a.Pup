612df2b539c235709bfcffd5e0b8fff09a969ea2
Philip <pcappelli901@gmail.com>
2018-04-14 16:26:59 -0400

    Adding MapAnnotations to MapView for the nearby Parks to the current users location

--- Pic.a.Pup/Controllers/MapViewController.swift
+++ Pic.a.Pup/Controllers/MapViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import SwiftLocation
 import MapKit
 import Alamofire
+import SwiftyJSON
 
 class MapViewController: UIViewController {
     
@@ -20,10 +21,6 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        
         
         Locator.currentPosition(accuracy: .neighborhood, onSuccess: { location in
             print(location)
@@ -32,30 +29,33 @@ class MapViewController: UIViewController {
             let lon = location.coordinate.longitude
             
             let parameters = ["key" : "AIzaSyBYCg8qITAAD2iPqPWfKM-Qi_UaM4urjWY",
-                              "location" : "38.031916,-75.471960",
-                              "radius" : "16000",
+                              "location" : "\(lat),\(lon)",
+                              "radius" : "1600",
                               "keyword" : "dog",
                               "type" : "park"]
             
             Alamofire.request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
-                              method: .get,
-                              parameters: parameters,
-                              //encoding: JSONEncoding.default,
-                              headers: nil)
-                .responseJSON { response in
-                    
-                    switch response.result {
-                    case .success(let JSON):
-                        let jsonDict = JSON as? NSDictionary ?? [:]
-                        print(jsonDict)
-                    case .failure(let error):
-                        print("Request failed with error: \(error)")
-                    }
+                              method: .get, parameters: parameters, headers: nil).responseJSON { response in
+                                switch response.result {
+                                case .success(let responseJSON):
+                                    //let jsonDict = JSON as? NSDictionary ?? [:]
+                                    let jsonDict = JSON(responseJSON)
+                                    if let results = jsonDict["results"].array {
+                                        print(results)
+                                        for places in results {
+                                            let name = places["name"].string
+                                            let lat = places["geometry"]["location"]["lat"].double
+                                            let lon = places["geometry"]["location"]["lng"].double
+                                            //print(lat, lon)
+                                            let coordinate = CLLocationCoordinate2D(latitude: lat!, longitude: lon!)
+                                            let dogPark = DogPark(title: name!, coordinate: coordinate)
+                                            self.mapView.addAnnotation(dogPark)
+                                            }
+                                        }
+                                case .failure(let error):
+                                    print("Request failed with error: \(error)")
+                                }
             }
-            
-            
-            
-            
         }, onFail: { error, location  in
             print(error)
         })
@@ -72,13 +72,9 @@ class MapViewController: UIViewController {
     
     let regionRadius: CLLocationDistance = 16000
     func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                  regionRadius, regionRadius)
-        
-        let dogPark = DogPark(title: "Poop", locationName: "really hairy", isOpen: true, coordinate: location.coordinate)
-        
-        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        //let dogPark = DogPark(title: "Poop", locationName: "really hairy", isOpen: true, coordinate: location.coordinate)
         mapView.setRegion(coordinateRegion, animated: true)
-        mapView.addAnnotation(dogPark)
+        //mapView.addAnnotation(dogPark)
     }
 }
--- Pic.a.Pup/Model/DogPark.swift
+++ Pic.a.Pup/Model/DogPark.swift
@@ -11,14 +11,10 @@ import MapKit
 
 class DogPark: NSObject, MKAnnotation {
     let title: String?
-    let locationName: String?
-    var isOpen: Bool?
     var coordinate: CLLocationCoordinate2D
     
-    init(title: String, locationName: String, isOpen: Bool, coordinate: CLLocationCoordinate2D) {
+    init(title: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
-        self.locationName = locationName
-        self.isOpen = isOpen
         self.coordinate = coordinate
         
         super.init()
